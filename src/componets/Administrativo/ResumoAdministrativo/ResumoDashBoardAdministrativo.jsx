5a0713fa8fbc77b4f98c702566530d99fefe8fab
Myltiane Alves <myltiane.aux@gmail.com>
2024-12-13 08:51:35 -0300

    feat: add ActionListaVisualizarDetalhe

--- src/componets/Administrativo/ResumoAdministrativo/ResumoDashBoardAdministrativo.jsx
+++ src/componets/Administrativo/ResumoAdministrativo/ResumoDashBoardAdministrativo.jsx
@@ -76,6 +76,7 @@ export const ResumoDashBoardAdministrativo = () => {
   }
 
   
+  
   const getListaAdiantamentoSalaria = async (empresaSelecionada) => {
     try {
       const response = await get(`/adiantamentos-salarial?idEmpresa=${empresaSelecionada}&dataPesquisa=${dataPesquisa}`);
