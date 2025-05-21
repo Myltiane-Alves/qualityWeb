2bc62b965e96ad8fc9d08aa7012e04564d5dcd90
Myltiane Alves <myltiane.aux@gmail.com>
2025-05-21 15:17:40 -0300

    REACTFOR: REMOVE TUDO

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import { RoutesMain } from './routes/Routes';
+import { DashBoardPromocao } from './pages/DashBoardPromocao';
 
 function App() {
+  const [componentToShow, setComponentToShow] = useState("");
+    const [usuarioLogado, setUsuarioLogado] = useState(null);
+  
+    useEffect(() => {
+      const usuarioArmazenado = localStorage.getItem('usuario');
+      if (usuarioArmazenado) {
+        const parsedUsuario = JSON.parse(usuarioArmazenado);
+        setUsuarioLogado(parsedUsuario);
+      }
+    }, []);
+  
+    useEffect(() => {
+  
+    }, [usuarioLogado]);
+  
+   
+  
+    const handleShowComponent = (componentName) => {
+      setComponentToShow(componentName);
+    };
   return (
-    <RoutesMain />
+    <DashBoardPromocao componentToShow={componentToShow} handleShowComponent={handleShowComponent} usuarioLogado={usuarioLogado} />
   )
 }
 
