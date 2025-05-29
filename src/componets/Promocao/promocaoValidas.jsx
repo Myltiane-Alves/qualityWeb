9d29b31c80e766806a0a1b6bdbe354c5c76cfc17
Myltiane Alves <myltiane.aux@gmail.com>
2025-05-29 09:33:48 -0300

    feat: criando regras de negócio para não permitir que estas mecanicas de promoção juntas

--- src/componets/Promocao/promocaoValidas.jsx
+++ src/componets/Promocao/promocaoValidas.jsx
@@ -1,15 +1,15 @@
-//  TPAPARTIRDE = 0 // aplicação destino por pares
-//  TPAPARTIRDE = 1 // aplicação destino em todos os produtos
-//  TPAPARTIRDE = 2 // aplicação destino no ultimo após entrada da promoção
-//  TPAPARTIRDE = 3 // aplicação destino menos na primeira
-//  TPAPARTIRDE = 4 // aplicação destino em 1(um) produto
+//  TPAPARTIRDE = 0  aplicação destino por pares
+//  TPAPARTIRDE = 1  aplicação destino em todos os produtos
+//  TPAPARTIRDE = 2  aplicação destino no ultimo após entrada da promoção
+//  TPAPARTIRDE = 3  aplicação destino menos na primeira
+//  TPAPARTIRDE = 4  aplicação destino em 1(um) produto
 
 // TABELAS RESUMOPROMOCAOMARKETING
-// TPAPLICADOA = 1->APLICADO A VALOR // 2-> APLICADO A QUANTIDADE
+// TPAPLICADOA = 1->APLICADO A VALOR  2-> APLICADO A QUANTIDADE
 // TPFATORPROMO = USADO PARA O PDV IDENTIFICAR A PROMOCAO
-// 	TPFATORPROMO = 0 // por valor final
-//  TPFATORPROMO = 1 // por valor desconto
-// 	TPFATORPROMO = 2 // por percentual desconto
+// 	TPFATORPROMO = 0  por valor final
+//  TPFATORPROMO = 1  por valor desconto
+// 	TPFATORPROMO = 2  por percentual desconto
 
 /*
   1. Primeira mecânica: Bloqueada não poderá funcionar juntas
