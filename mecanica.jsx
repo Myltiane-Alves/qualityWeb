497ac66fba9f7cf60c9429899a4ab93a5d039be1
Myltiane Alves <myltiane.aux@gmail.com>
2025-06-04 14:17:34 -0300

    refactor: promocao mudando lógica das mecanica

--- mecanica.jsx
+++ mecanica.jsx
@@ -1,169 +1,167 @@
 
-    export const optionsMecanica = [
-        {
-            value: 1,
-            label: "PROMOÇÃO POR PARES // QUANTIDADE // PERCENTUAL DESCONTO",
-            aplicacaoDestino: 0,
-            mecanica: 2,
-            tipoDesconto: 2,
-            color: "blue"
-        },
-        {
-            value: 2,
-            label: "PROMOÇÃO POR TODOS OS PRODUTOS // QUANTIDADE // PERCENTUAL DESCONTO",
-            aplicacaoDestino: 1,
-            mecanica: 2,
-            tipoDesconto: 2,
-            color: "green"
-        },
-        {
-            value: 3,
-            label: "PROMOÇÃO POR MENOS NA PRIMEIRA // QUANTIDADE // PERCENTUAL DESCONTO",
-            aplicacaoDestino: 3,
-            mecanica: 2,
-            tipoDesconto: 2,
-            color: "yellowGreen"
-        },
-        {
-            value: 4,
-            label: "PROMOÇÃO POR EM UM PRODUTO // QUANTIDADE // PERCENTUAL DESCONTO",
-            aplicacaoDestino: 4,
-            mecanica: 2,
-            tipoDesconto: 2,
-            color: "purple"
-        },
+export const optionsMecanica = [
+    {
+        value: 1,
+        label: "PROMOÇÃO POR PARES // QUANTIDADE // PERCENTUAL DESCONTO",
+        aplicacaoDestino: 0,
+        mecanica: 2,
+        tipoDesconto: 2,
+        color: "blue"
+    },
+    {
+        value: 2,
+        label: "PROMOÇÃO POR TODOS OS PRODUTOS // QUANTIDADE // PERCENTUAL DESCONTO",
+        aplicacaoDestino: 1,
+        mecanica: 2,
+        tipoDesconto: 2,
+        color: "green"
+    },
+    {
+        value: 3,
+        label: "PROMOÇÃO POR MENOS NA PRIMEIRA // QUANTIDADE // PERCENTUAL DESCONTO",
+        aplicacaoDestino: 3,
+        mecanica: 2,
+        tipoDesconto: 2,
+        color: "yellowGreen"
+    },
+    {
+        value: 4,
+        label: "PROMOÇÃO POR EM UM PRODUTO // QUANTIDADE // PERCENTUAL DESCONTO",
+        aplicacaoDestino: 4,
+        mecanica: 2,
+        tipoDesconto: 2,
+        color: "purple"
+    },
 
-        {
-            value: 5,
-            label: "PROMOÇÃO POR PARES // VALOR // PERCENTUAL DESCONTO",
-            aplicacaoDestino: 0,
-            mecanica: 1,
-            tipoDesconto: 2,
-            color: "orange"
-        },
-        {
-            value: 6,
-            label: "PROMOÇÃO POR TODOS OS PRODUTOS // VALOR // PERCENTUAL DESCONTO",
-            aplicacaoDestino: 1,
-            mecanica: 1,
-            tipoDesconto: 2,
-            color: "pink"
-        },
-        {
-            value: 7,
-            label: "PROMOÇÃO POR MENOS NA PRIMEIRA // VALOR // PERCENTUAL DESCONTO",
-            aplicacaoDestino: 3,
-            mecanica: 1,
-            tipoDesconto: 2,
-            color: "brown"
-        },
-        {
-            value: 8,
-            label: "PROMOÇÃO POR EM UM PRODUTO // VALOR // PERCENTUAL DESCONTO",
-            aplicacaoDestino: 4,
-            mecanica: 1,
-            tipoDesconto: 2,
-            color: "gray"
-        },
+    {
+        value: 5,
+        label: "PROMOÇÃO POR PARES // VALOR // PERCENTUAL DESCONTO",
+        aplicacaoDestino: 0,
+        mecanica: 1,
+        tipoDesconto: 2,
+        color: "orange"
+    },
+    {
+        value: 6,
+        label: "PROMOÇÃO POR TODOS OS PRODUTOS // VALOR // PERCENTUAL DESCONTO",
+        aplicacaoDestino: 1,
+        mecanica: 1,
+        tipoDesconto: 2,
+        color: "pink"
+    },
+    {
+        value: 7,
+        label: "PROMOÇÃO POR MENOS NA PRIMEIRA // VALOR // PERCENTUAL DESCONTO",
+        aplicacaoDestino: 3,
+        mecanica: 1,
+        tipoDesconto: 2,
+        color: "brown"
+    },
+    {
+        value: 8,
+        label: "PROMOÇÃO POR EM UM PRODUTO // VALOR // PERCENTUAL DESCONTO",
+        aplicacaoDestino: 4,
+        mecanica: 1,
+        tipoDesconto: 2,
+        color: "gray"
+    },
 
-        {
-            value: 9,
-            label: "PROMOÇÃO POR PARES // VALOR // VALOR DESCONTO",
-            aplicacaoDestino: 0,
-            mecanica: 1,
-            tipoDesconto: 1,
-            color: "teal"
-        },
-        {
-            value: 10,
-            label: "PROMOÇÃO POR TODOS OS PRODUTOS // VALOR // VALOR DESCONTO",
-            aplicacaoDestino: 1,
-            mecanica: 1,
-            tipoDesconto: 1,
-            color: "lime"
-        },
-        {
-            value: 11,
-            label: "PROMOÇÃO POR MENOS NA PRIMEIRA // VALOR // VALOR DESCONTO",
-            aplicacaoDestino: 3,
-            mecanica: 1,
-            tipoDesconto: 1,
-            color: "maroon"
-        },
-        {
-            value: 12,
-            label: "PROMOÇÃO POR EM UM PRODUTO // VALOR // VALOR DESCONTO",
-            aplicacaoDestino: 4,
-            mecanica: 1,
-            tipoDesconto: 1,
-            color: "olive"
-        },
+    {
+        value: 9,
+        label: "PROMOÇÃO POR PARES // VALOR // VALOR DESCONTO",
+        aplicacaoDestino: 0,
+        mecanica: 1,
+        tipoDesconto: 1,
+        color: "teal"
+    },
+    {
+        value: 10,
+        label: "PROMOÇÃO POR TODOS OS PRODUTOS // VALOR // VALOR DESCONTO",
+        aplicacaoDestino: 1,
+        mecanica: 1,
+        tipoDesconto: 1,
+        color: "lime"
+    },
+    {
+        value: 11,
+        label: "PROMOÇÃO POR MENOS NA PRIMEIRA // VALOR // VALOR DESCONTO",
+        aplicacaoDestino: 3,
+        mecanica: 1,
+        tipoDesconto: 1,
+        color: "maroon"
+    },
+    {
+        value: 12,
+        label: "PROMOÇÃO POR EM UM PRODUTO // VALOR // VALOR DESCONTO",
+        aplicacaoDestino: 4,
+        mecanica: 1,
+        tipoDesconto: 1,
+        color: "olive"
+    },
 
-        {
-            value: 13,
-            label: "PROMOÇÃO POR PARES // QUANTIDADE // VALOR FINAL",
-            aplicacaoDestino: 0,
-            mecanica: 2,
-            tipoDesconto: 0,
-            color: "silver"
-        },
-        {
-            value: 14,
-            label: "PROMOÇÃO POR TODOS OS PRODUTOS // QUANTIDADE // VALOR FINAL",
-            aplicacaoDestino: 1,
-            mecanica: 2,
-            tipoDesconto: 0,
-            color: "blueviolet"
-        },
-        {
-            value: 15,
-            label: "PROMOÇÃO POR MENOS NA PRIMEIRA // QUANTIDADE // VALOR FINAL",
-            aplicacaoDestino: 3,
-            mecanica: 2,
-            tipoDesconto: 0,
-            color: "limegreen"
-        },
-        {
-            value: 16,
-            label: "PROMOÇÃO POR EM UM PRODUTO // QUANTIDADE // VALOR FINAL",
-            aplicacaoDestino: 4,
-            mecanica: 2,
-            tipoDesconto: 0,
-            color: "coral"
-        },
+    {
+        value: 13,
+        label: "PROMOÇÃO POR PARES // QUANTIDADE // VALOR FINAL",
+        aplicacaoDestino: 0,
+        mecanica: 2,
+        tipoDesconto: 0,
+        color: "silver"
+    },
+    {
+        value: 14,
+        label: "PROMOÇÃO POR TODOS OS PRODUTOS // QUANTIDADE // VALOR FINAL",
+        aplicacaoDestino: 1,
+        mecanica: 2,
+        tipoDesconto: 0,
+        color: "blueviolet"
+    },
+    {
+        value: 15,
+        label: "PROMOÇÃO POR MENOS NA PRIMEIRA // QUANTIDADE // VALOR FINAL",
+        aplicacaoDestino: 3,
+        mecanica: 2,
+        tipoDesconto: 0,
+        color: "limegreen"
+    },
+    {
+        value: 16,
+        label: "PROMOÇÃO POR EM UM PRODUTO // QUANTIDADE // VALOR FINAL",
+        aplicacaoDestino: 4,
+        mecanica: 2,
+        tipoDesconto: 0,
+        color: "coral"
+    },
 
-        {
-            value: 17,
-            label: "PROMOÇÃO POR PARES // QUANTIDADE // VALOR DESCONTO",
-            aplicacaoDestino: 0,
-            mecanica: 2,
-            tipoDesconto: 1,
-            color: "indigo"
-        },
-        {
-            value: 18,
-            label: "PROMOÇÃO POR TODOS OS PRODUTOS // QUANTIDADE // VALOR DESCONTO",
-            aplicacaoDestino: 1,
-            mecanica: 2,
-            tipoDesconto: 1,
-            color: "crimson"
-        },
-        {
-            value: 19,
-            label: "PROMOÇÃO POR MENOS NA PRIMEIRA // QUANTIDADE // VALOR DESCONTO",
-            aplicacaoDestino: 3,
-            mecanica: 2,
-            tipoDesconto: 1,
-            color: "darkviolet"
-        },
-        {
-            value: 20,
-            label: "PROMOÇÃO POR EM UM PRODUTO // QUANTIDADE // VALOR DESCONTO",
-            aplicacaoDestino: 4,
-            mecanica: 2,
-            tipoDesconto: 1,
-            color: "darkslategray"
-        }
-    ]
-
-   
+    {
+        value: 17,
+        label: "PROMOÇÃO POR PARES // QUANTIDADE // VALOR DESCONTO",
+        aplicacaoDestino: 0,
+        mecanica: 2,
+        tipoDesconto: 1,
+        color: "indigo"
+    },
+    {
+        value: 18,
+        label: "PROMOÇÃO POR TODOS OS PRODUTOS // QUANTIDADE // VALOR DESCONTO",
+        aplicacaoDestino: 1,
+        mecanica: 2,
+        tipoDesconto: 1,
+        color: "crimson"
+    },
+    {
+        value: 19,
+        label: "PROMOÇÃO POR MENOS NA PRIMEIRA // QUANTIDADE // VALOR DESCONTO",
+        aplicacaoDestino: 3,
+        mecanica: 2,
+        tipoDesconto: 1,
+        color: "darkviolet"
+    },
+    {
+        value: 20,
+        label: "PROMOÇÃO POR EM UM PRODUTO // QUANTIDADE // VALOR DESCONTO",
+        aplicacaoDestino: 4,
+        mecanica: 2,
+        tipoDesconto: 1,
+        color: "darkslategray"
+    }
+]
