8ac96b47872e9d6e37aae76a8550920cd0ac673a
Myltiane Alves <myltiane.aux@gmail.com>
2025-07-21 14:20:57 -0300

    refactor: ActionMainPromocao select

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const BASE_URL = "http://164.152.244.96:6001";
 
-const BASE_URL = "https://api-quality.vercel.app";
+const BASE_URL = "https://backend-producao.vercel.app";
 // const BASE_URL = "http://localhost:6001";
 
 
