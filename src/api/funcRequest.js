c0f7ef6974efa1390a3d912824a4e268c442b2b1
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-02 10:54:13 -0300

    refactor:

--- src/api/funcRequest.js
+++ src/api/funcRequest.js
@@ -3,7 +3,6 @@ import api from "./api";
 export const post = async (endpoint, data) => {
   try {
     // const token = localStorage.getItem('token');
-
     const response = await api.post(endpoint, data, {
       headers: {
         // Authorization: `Bearer ${token}`,
